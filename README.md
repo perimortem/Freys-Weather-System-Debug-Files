3b87c5ae327e0e1a0b7fca84892faed0cde1f18f
perimortem <a.devivyeiii@gmail.com>
2022-05-17 12:14:06 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Freys Weather System Debug Files
+ These files are edited to solve the complier issues found in Freys Weather Systems for Unreal Engine.
---
+++ WaterBase.h
@@ -0,0 +1,39 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+// publisher name:Frey
+// 2021 of publication
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/SceneComponent.h"
+#include "Engine/StaticMesh.h"
+#include "Materials/MaterialInstance.h"
+#include "Materials/Material.h"
+#include "WaterBase.generated.h"
+
+UCLASS()
+class WEATHERSYSTEM_API AWaterBase : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AWaterBase();
+	//根组件
+	class USceneComponent* RootComponent;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Unique)
+	class UMaterialInstance* WaterMaterial;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Unique)
+	class UStaticMeshComponent* WaterComp;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Unique)
+	class UStaticMesh* WaterMesh;
+	void SetWaterBloackAll();
+	void SetWaterNoCollision();
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+};
